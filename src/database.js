0534876c8a20e6da593ea679a3876d03ed17f88d
DavidCornejoB <luisdavidcorbra24@gmail.com>
2023-10-12 13:37:33 -0500

    Comunicación con base de datos mysql en la Nube

--- src/database.js
+++ src/database.js
@@ -1,11 +1,12 @@
 import { createPool } from 'mysql2/promise';
 
 const pool = createPool({
-    host: 'localhost',
+    host: 'sql9.freemysqlhosting.net',
     port: '3306',
-    user: 'root',
-    password: 'root',
-    database: 'Prueba01'
+    user: 'sql9652888',
+    password: '5xp5THRip9',
+    database: 'sql9652888',
+    
 });
 
 export default pool;
